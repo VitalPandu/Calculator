ef69e423f9631ec58f15965dba179b4524d05503
VitalPandu <yalamarthipandurangavital@gmail.com>
2021-06-17 21:00:18 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var input1 = Number(document.getElementById('input1').value);
+var input2 = Number(document.getElementById('input2').value);
+var o;
+function add(){
+document.getElementById('output').value= `${input1}`+`${input2}`;
+
+}
